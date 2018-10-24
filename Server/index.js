c5bfd4c7ef1df20682eff1616336fb92da31000d
Pietro Caselani <pc1992@gmail.com>
2018-10-23 21:48:46 -0300

    Handle missing env var SLACK_TOKEN

--- Server/index.js
+++ Server/index.js
@@ -2,6 +2,12 @@ const Server = require('./lib/server');
 const Slack = require('slack');
 
 const token = process.env.SLACK_TOKEN;
+
+if (!token) {
+  console.log('Missing env var SLACK_TOKEN');
+  process.exit(1);
+}
+
 const slack = new Slack({token});
 
 Server.start(slack);
