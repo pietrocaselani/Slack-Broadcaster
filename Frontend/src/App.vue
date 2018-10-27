11596e64bb933ae22efc825540d3e24c02a184a9
Pietro Caselani <pc1992@gmail.com>
2018-10-27 20:28:01 -0300

    Add text input and button, but no actions

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,8 +1,13 @@
 <template>
   <div id="app">
+    <textarea v-model="message" placeholder="Digite a mensagem"></textarea>
     <li v-for="channel in channels" v-bind:key="channel.id">
       <input type="checkbox"> {{ channel.name }}
     </li>
+    <button v-on:click="broadcast">Enviar mensagem</button>
+
+    <br />
+    <span>Status:</span> <p style="white-space: pre-line;">{{ status }}</p>
   </div>
 </template>
 
@@ -13,7 +18,16 @@ import api from '@/api';
 export default {
   name: 'app',
   data() {
-    return {channels: []}
+    return {
+      message: "",
+      status: "Não enviada",
+      channels: [],
+    }
+  },
+  methods: {
+    broadcast: function() {
+      this.status = `Mensagem "${this.message}" enviada`
+    }
   },
   created() {
     api.get_public_channels().then(response => {
